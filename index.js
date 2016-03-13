6c4a57daa3489698517845b97153f3fd95f2a87b
luisiam <luisiam@gmail.com>
2016-03-13 14:04:32 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,105 +9,104 @@ module.exports = function(homebridge){
 
 function cmdSwitchAccessory(log, config){
 
-	// Retrieve existing config
-	this.log = log;
-	this.name = config.name;
-	this.on_cmd = config.on_cmd || "";
-	this.off_cmd = config.off_cmd || "";
-	this.state_cmd = config.state_cmd || "";
-	this.manufacturer = config.manufacturer;
-	this.model = config.model;
-	this.serial = config.serial;
-	if (config.default == "on")	this.state = true;
-	else				this.state = false;
+  // Retrieve existing config
+  this.log = log;
+  this.name = config.name;
+  this.on_cmd = config.on_cmd || "";
+  this.off_cmd = config.off_cmd || "";
+  this.state_cmd = config.state_cmd || "";
+  this.manufacturer = config.manufacturer;
+  this.model = config.model;
+  this.serial = config.serial;
+  if (config.default == "on") this.state = true;
+  else this.state = false;
 }
 
 cmdSwitchAccessory.prototype = {
 
-	// Method to determine current state
-	getPowerState: function(callback) {
-		var that = this;
+  // Method to determine current state
+  getPowerState: function(callback) {
+    var that = this;
 
-		// Execute command to detect state
-		if (this.state_cmd) {
-			exec(this.state_cmd, function(error, stdout, stderr) {
-				that.state = stdout ? true : false;
-				that.log("Current state: " + (that.state ? "on." : "off."));
-				callback(null, that.state);
-			});
-		} else {
-			this.log("Current state: " + (this.state ? "on" : "off") + " (cached).");
-			callback(null, this.state);
-		}
-	},
+    // Execute command to detect state
+    if (this.state_cmd) {
+      exec(this.state_cmd, function(error, stdout, stderr) {
+        that.state = stdout ? true : false;
+        that.log("Current state: " + (that.state ? "on." : "off."));
+        callback(null, that.state);
+      });
+    } else {
+      this.log("Current state: " + (this.state ? "on" : "off") + " (cached).");
+      callback(null, this.state);
+    }
+  },
 
-	// Method to set state
-	setPowerState: function(on, callback) {
-		var that = this;
-		var cmd = on ? this.on_cmd : this.off_cmd;
-		var tout = null;
+  // Method to set state
+  setPowerState: function(on, callback) {
+    var that = this;
+    var cmd = on ? this.on_cmd : this.off_cmd;
+    var tout = null;
 
-		// Execute command to set state
-		if (cmd) {
-			exec(cmd, function(error, stdout, stderr) {
+    // Execute command to set state
+    if (cmd) {
+      exec(cmd, function(error, stdout, stderr) {
 
-				// Error detection
-				if (error) {
-					that.log(error);
-					that.log(stderr);
-					that.log("Failed to turn " + (on ? "on " : "off ") + "!");
-					that.state = !on;
-				} else {
-					that.log("Turned " + (on ? "on." : "off."));
-					that.state = on;
-				}
+        // Error detection
+        if (error) {
+          that.log(error);
+          that.log(stderr);
+          that.log("Failed to turn " + (on ? "on " : "off ") + "!");
+          that.state = !on;
+        } else {
+          that.log("Turned " + (on ? "on." : "off."));
+          that.state = on;
+        }
 
-				// Work-around: always return success
-				// Rely on getPowerState to check final state
-				if (tout) {
-					clearTimeout(tout);
-					callback();
-				}
-			});
+        // Work-around: always return success
+        // Rely on getPowerState to check final state
+        if (tout) {
+          clearTimeout(tout);
+          callback();
+        }
+      });
 
-			// Allow 2s to set state but otherwise assumes success
-			tout = setTimeout(function() {
-				tout = null;
-				that.log("Turning " + (on ? "on " : "off ") + " took too long, assuming success." );
-				callback();
-			}, 2000);
-		} else {
-			this.log("Turned " + (on ? "on" : "off") + " (dummy switch).");
-			this.state = on;
-			callback();
-		}
-	},
+      // Allow 2s to set state but otherwise assumes success
+      tout = setTimeout(function() {
+        tout = null;
+        that.log("Turning " + (on ? "on " : "off ") + " took too long, assuming success." );
+        callback();
+      }, 2000);
+    } else {
+      this.log("Turned " + (on ? "on" : "off") + " (dummy switch).");
+      this.state = on;
+      callback();
+    }
+  },
 
-	// Method to respond identify request
-	identify: function(callback) {
-		this.log("Identify requested!");
-		callback();
-	},
+  // Method to respond identify request
+  identify: function(callback) {
+    this.log("Identify requested!");
+    callback();
+  },
 
 	// Method to return existing services
-	getServices: function() {
+  getServices: function() {
 
-		// Create Accessory Informaton Service
-		var informationService = new Service.AccessoryInformation();
+    // Create Accessory Informaton Service
+    var informationService = new Service.AccessoryInformation();
+    if (this.manufacturer) informationService.setCharacteristic(Characteristic.Manufacturer, this.manufacturer);
+    if (this.model) informationService.setCharacteristic(Characteristic.Model, this.model);
+    if (this.serial) informationService.setCharacteristic(Characteristic.SerialNumber, this.serial);
 
-		if (this.manufacturer) informationService.setCharacteristic(Characteristic.Manufacturer, this.manufacturer);
-		if (this.model) informationService.setCharacteristic(Characteristic.Model, this.model);
-		if (this.serial) informationService.setCharacteristic(Characteristic.SerialNumber, this.serial);
+    // Create a Switch
+    var switchService = new Service.Switch(this.name)
+    switchService.getCharacteristic(Characteristic.On)
+      .on('get', this.getPowerState.bind(this))
+      .on('set', this.setPowerState.bind(this));
 
-		// Create a Switch
-		var switchService = new Service.Switch(this.name)
-		switchService.getCharacteristic(Characteristic.On)
-			.on('get', this.getPowerState.bind(this))
-			.on('set', this.setPowerState.bind(this));
+    // Get Initial State
+    switchService.getCharacteristic(Characteristic.On).getValue();
 
-		// Get Initial State
-		switchService.getCharacteristic(Characteristic.On).getValue();
-
-		return [informationService, switchService];
-	}
+    return [informationService, switchService];
+  }
 };
